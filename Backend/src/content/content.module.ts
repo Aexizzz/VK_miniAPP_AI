5adc28724e1771fb55e718973533730adc482ec5
Aexizzz <ammelihov@gmail.com>
2025-12-13 22:07:55 +0500

    fix_build

--- Backend/src/content/content.module.ts
+++ Backend/src/content/content.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ContentService } from './content.service';
 import { ContentController } from './content.controller';
+import { VkModule } from '../vk/vk.module';
 
 @Module({
+  imports: [VkModule],
   controllers: [ContentController],
   providers: [ContentService],
 })
